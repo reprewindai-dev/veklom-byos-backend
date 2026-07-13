cfbf3a7efcb0ab6f7055249918d9357de05ed0a4
reprewindai-dev <reprewindai@gmail.com>
2026-07-13 13:12:39 -0400

    fix(db): merge concurrent Alembic heads and add CI gate

---
+++ backend/db/migrations/versions/20260713_1311_4a6c552e87de_merge_concurrent_migration_heads.py
@@ -0,0 +1,26 @@
+"""merge concurrent migration heads
+
+Revision ID: 4a6c552e87de
+Revises: 5340720ec7f0, drop_repo_gate_1234
+Create Date: 2026-07-13 13:11:29.402616
+
+"""
+from typing import Sequence, Union
+
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision: str = '4a6c552e87de'
+down_revision: Union[str, None] = ('5340720ec7f0', 'drop_repo_gate_1234')
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
